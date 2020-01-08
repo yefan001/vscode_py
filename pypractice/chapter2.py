7669204000c78fbce25cf4378003207bd01e9ea8
叶凡 <13920338326@163.com>
2020-01-08 12:18:52 +0800

    practice

---
+++ pypractice/chapter2.py
@@ -0,0 +1,10 @@
+# 变量
+""" message='hello world!'
+print(message) """
+#字符串
+name='ada lovelace'
+print(name.title())     #首字母大写
+print(name.upper())     #转大写
+print(name.lower())     #转小写        
+
+
